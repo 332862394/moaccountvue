029a713a475026019b617936e6178ce46691208d
luzh <332862394@qq.com>
2021-12-27 16:29:19 +0800

    重构代码

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -103,7 +103,8 @@ export default class Statistics extends Vue {
     };
   }
   mounted() {
-    (this.$refs.chartWrapper as HTMLDivElement).scrollLeft = 9999;
+    const div = this.$refs.chartWrapper as HTMLDivElement;
+    div.scrollLeft = div.scrollWidth;
   }
   beautify(string: string) {
     const day = dayjs(string);
