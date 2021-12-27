1908650c1419457bfae757ee8673cf6d07e580bc
luzh <332862394@qq.com>
2021-12-27 09:42:32 +0800

    完成列表的css

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,7 @@ import createId from "../lib/createId";
 import router from "../router/index";
 
 Vue.use(Vuex); //把store绑到Vue.prototype.$store=store
-type RootState = {
-  recordList: RecordItem[];
-  tagList: Tag[];
-  currentTag?: Tag;
-};
+
 const store = new Vuex.Store({
   state: {
     recordList: [],
@@ -24,7 +20,7 @@ const store = new Vuex.Store({
     },
     createRecord(state, record: RecordItem) {
       const record2: RecordItem = clone(record);
-      record2.createdAt = new Date();
+      record2.createdAt = new Date().toISOString();
       state.recordList.push(record2);
       store.commit("saveRecord");
     },
